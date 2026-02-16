46399bdc552865c74ad15293f42ca9a8b654c87f
= <=>
2026-02-16 20:45:46 +0700

    Init repo

---
+++ main.py
@@ -0,0 +1,38 @@
+import pygame
+from asteroid import Asteroid
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+from player import Player
+from asteroidfield import AsteroidField
+
+
+def main():
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    me = Player(x, y)
+    field = AsteroidField()
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+
+if __name__ == "__main__":
+    main()
